6864b906b9c9309ae6e969b4626aa18caf0fd154
Chris jurrens <crjurrens@gmail.com>
2019-03-26 15:11:45 -0500

    Working on Coordinate

--- Chris_Parker_Assign4/Chris_Parker_Assign4/Form1.Designer.cs
+++ Chris_Parker_Assign4/Chris_Parker_Assign4/Form1.Designer.cs
@@ -28,39 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.CoordinatePlane = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CoordinatePlane)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // CoordinatePlane
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.CoordinatePlane.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.BackColor = System.Drawing.Color.Black;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.CoordinatePlane.BackColor = System.Drawing.Color.Black;
+            this.CoordinatePlane.Location = new System.Drawing.Point(12, 12);
+            this.CoordinatePlane.Name = "CoordinatePlane";
+            this.CoordinatePlane.Size = new System.Drawing.Size(500, 500);
+            this.CoordinatePlane.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.CoordinatePlane.TabIndex = 0;
+            this.CoordinatePlane.TabStop = false;
+            this.CoordinatePlane.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawAxes);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(839, 570);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CoordinatePlane);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoordinatePlane)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox CoordinatePlane;
     }
 }
 
--- Chris_Parker_Assign4/Chris_Parker_Assign4/Form1.cs
+++ Chris_Parker_Assign4/Chris_Parker_Assign4/Form1.cs
@@ -12,9 +12,24 @@ namespace Chris_Parker_Assign4
 {
     public partial class Form1 : Form
     {
+        private static Pen whitePen;
+
         public Form1()
         {
             InitializeComponent();
+
+            whitePen = new Pen(Color.White);
+           
+        }
+
+        private void DrawAxes(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            //Horizontal Axis
+            g.DrawLine(whitePen, 0, (CoordinatePlane.Height / 2), CoordinatePlane.Width, (CoordinatePlane.Height / 2));
+            //Vertical Axis
+            g.DrawLine(whitePen, (CoordinatePlane.Width / 2), 0, (CoordinatePlane.Width / 2), CoordinatePlane.Height);
         }
     }
 }
