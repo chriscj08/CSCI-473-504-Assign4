9d834830db78afe3362e5e85d34fe187144f6af3
Chris jurrens <crjurrens@gmail.com>
2019-03-27 14:39:02 -0500

    Made progress on linear equations.

--- Chris_Parker_Assign4/Chris_Parker_Assign4/Form1.cs
+++ Chris_Parker_Assign4/Chris_Parker_Assign4/Form1.cs
@@ -13,6 +13,10 @@ namespace Chris_Parker_Assign4
     public partial class Form1 : Form
     {
         private static Pen whitePen;
+        private static int xMax = 4;
+        private static int xMin = -8;
+        private static int yMax = 7;
+        private static int yMin = -6;
 
         public Form1()
         {
@@ -27,9 +31,62 @@ namespace Chris_Parker_Assign4
             Graphics g = e.Graphics;
 
             //Horizontal Axis
-            g.DrawLine(whitePen, 0, (CoordinatePlane.Height / 2), CoordinatePlane.Width, (CoordinatePlane.Height / 2));
+            g.DrawLine(whitePen, 0, ( Math.Abs(yMin) * CoordinatePlane.Height / Math.Abs(yMin) + Math.Abs(yMax)), CoordinatePlane.Width, (Math.Abs(yMin) * CoordinatePlane.Height / 13));
             //Vertical Axis
-            g.DrawLine(whitePen, (CoordinatePlane.Width / 2), 0, (CoordinatePlane.Width / 2), CoordinatePlane.Height);
+            g.DrawLine(whitePen, (Math.Abs(xMin) * CoordinatePlane.Width / Math.Abs(xMin) + Math.Abs(xMax)), 0, (Math.Abs(xMin) * CoordinatePlane.Width / Math.Abs(xMin) + Math.Abs(xMax)), CoordinatePlane.Height);
+
+            //Finding x1 and x2
+            int result = (-2 * xMin) + 3; 
+            int x1 = 0;
+            int y1 = 0;
+            int y2 = 0;
+
+            
+            for (x1 = xMin; result > yMax; x1++)
+            {
+                result = (-2 * x1) + 3;
+            }
+
+            x1--;
+
+            x1 = Math.Abs(xMin - x1);
+
+            if (result == yMax)
+            {
+                y1 = 0;
+            }
+            else
+            {
+                x1 = 0;
+                y1 = Math.Abs(yMax - result);
+            }
+
+            result = (-2 * xMax) + 3;
+            int x2 = 0;
+
+            for (x2 = xMax; result < yMin; x2--)
+            {
+                result = (-2 * x2) + 3;
+            }
+
+            if (result == yMin)
+            {
+                y2 = Math.Abs(yMin) + Math.Abs(yMax); ;
+            }
+            else
+            {
+                x2 = Math.Abs(xMin) + Math.Abs(xMax); 
+                y2 = Math.Abs(yMax) + Math.Abs(result);
+            }
+            //Found x1 and x2
+
+            MessageBox.Show(x1.ToString() + " : " + x2.ToString() + " : " + y1.ToString() + " : " + y2.ToString() + " : ");
+
+            int denominator1 = Math.Abs(xMin) + Math.Abs(xMax);
+            int denominator2 = Math.Abs(yMin) + Math.Abs(yMax);
+            
+            g.DrawLine(whitePen, (Math.Abs(x1) * CoordinatePlane.Width / denominator1), (Math.Abs(y1) * CoordinatePlane.Height / denominator2),
+                      (Math.Abs(x2) * CoordinatePlane.Width / denominator1), (Math.Abs(y2) * CoordinatePlane.Height / denominator2));
         }
 
         private void Form1_Load(object sender, EventArgs e)
